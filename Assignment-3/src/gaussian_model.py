6b506a0ba1e253fd8b092d2b0bc131a6d25f3b63
malak_sherif <malokasheko@gmail.com>
2025-12-07 16:51:20 +0200

    modified things in probability class

--- Assignment-3/src/gaussian_model.py
+++ Assignment-3/src/gaussian_model.py
@@ -6,25 +6,27 @@ from numpy.linalg import slogdet, inv
 class GaussianGenerativeModel:
     def __init__(self, lambda_reg=1e-3):
         self.lambda_reg = lambda_reg
-
+    #μ_k is the mean vector for class k (a 64-dimensional vector) 
     def fit(self, X, y):
+        #hena bn define each class k in {0, 1, …, 9}
         self.classes = np.unique(y)
         self.K = len(self.classes)
         self.d = X.shape[1]
-
+        #  N(x ; μ_k, Σ)
         N = X.shape[0]
 
-        # PRIORS
+        # -> PRIORS : BNGEEB probability kol digit ll training set
         self.pi = np.zeros(self.K)
         for k in self.classes:
             self.pi[k] = np.sum(y == k) / N
 
-        # MEANS
+        # -> MEANS : BNGEEB ll 64_ D el mean 
         self.mu = np.zeros((self.K, self.d))
         for k in self.classes:
             self.mu[k] = np.mean(X[y == k], axis=0)
 
-        # SHARED COVARIANCE
+        # -> SHARED COVARIANCE : bngeeb el average ll points 
+        #Accumulate (x_i – μ_{y_i}) (x_i – μ_{y_i})^T
         S = np.zeros((self.d, self.d))
         for i in range(N):
             k = y[i]
@@ -32,13 +34,15 @@ class GaussianGenerativeModel:
             S += diff @ diff.T
         S /= N
 
-        # REGULARIZATION
+        # -> REGULARIZATION : bn classify new points by inverse Σ−1 w log determination log∣Σ∣
+        # Σ_λ = Σ + λ I
         self.Sigma = S + self.lambda_reg * np.eye(self.d)
         self.Sigma_inv = inv(self.Sigma)
         sign, logdet = slogdet(self.Sigma)
         self.logdetSigma = logdet
 
     def _log_gaussian(self, x, k):
+        # log p(y = k | x) ∝ log π_k + log N(x ; μ_k, Σ_λ) for prediction
         diff = (x - self.mu[k])
         t1 = -0.5 * (diff.T @ self.Sigma_inv @ diff)
         t2 = -0.5 * (self.d * np.log(2 * np.pi) + self.logdetSigma)
