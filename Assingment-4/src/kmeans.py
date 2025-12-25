40f62d8c0b97e0e14b6afcc59b8299826038631c
fai1205 <fai_raafat@hotmail.com>
2025-12-25 03:39:27 +0200

    modify kmeans

--- Assingment-4/src/kmeans.py
+++ Assingment-4/src/kmeans.py
@@ -2,13 +2,13 @@ import numpy as np
 
 class NumpyKMeans:
     def __init__(self, n_clusters=8, init='k-means++', max_iter=300, tol=1e-4, random_state=None):
-        self.n_clusters = n_clusters
-        self.init = init
+        self.n_clusters = n_clusters # k
+        self.init = init # k-means++ / random
         self.max_iter = max_iter
-        self.tol = tol
+        self.tol = tol # tolerance on inertia change
         self.random_state = random_state
-        self.cluster_centers_ = None
-        self.labels_ = None
+        self.cluster_centers_ = None # final centroid coordinates after fit
+        self.labels_ = None 
         self.inertia_ = None
         self.n_iter_ = None
         self.inertia_history_ = None
@@ -45,28 +45,34 @@ class NumpyKMeans:
         # Initialize centers
         self.cluster_centers_ = self._initialize_centers(X)
         inertia_history = []
+        labels_old = None
         
         for i in range(self.max_iter):
-            # Vectorized assignment
             dists_to_centers = np.sum((X[:, np.newaxis, :] - self.cluster_centers_[np.newaxis, :, :])**2, axis=2)
-            labels_old = self.labels_
+            
             self.labels_ = np.argmin(dists_to_centers, axis=1)
             
-            # Update centers
-            new_centers = np.array([X[self.labels_ == k].mean(axis=0) 
-                                  if np.any(self.labels_ == k) else self.cluster_centers_[k]
-                                  for k in range(self.n_clusters)])
+            new_centers = np.zeros((self.n_clusters, X.shape[1]))
+            for k in range(self.n_clusters):
+                mask = (self.labels_ == k)
+                if np.any(mask):
+                    new_centers[k] = X[mask].mean(axis=0)
+                else:
+                    new_centers[k] = self.cluster_centers_[k]  # Keep old if empty
+            
             self.cluster_centers_ = new_centers
             
-            # Vectorized inertia computation
-            inertia = np.sum(dists_to_centers[self.labels_, np.arange(len(self.labels_))])
+            dists_to_new_centers = np.sum((X[:, np.newaxis, :] - self.cluster_centers_[np.newaxis, :, :])**2, axis=2)
+            inertia = np.sum(dists_to_centers[np.arange(len(self.labels_)), self.labels_])
             inertia_history.append(inertia)
             
-            # Check convergence
+            #  Convergence checks
             if labels_old is not None and np.all(labels_old == self.labels_):
                 break
             if i > 0 and np.abs(inertia_history[-2] - inertia) < self.tol:
                 break
+                
+            labels_old = self.labels_.copy()
         
         self.inertia_ = inertia
         self.n_iter_ = i + 1
@@ -76,7 +82,6 @@ class NumpyKMeans:
     def predict(self, X):
         if self.cluster_centers_ is None:
             raise ValueError("Model not fitted yet. Call fit() first.")
-        
         X = np.array(X, dtype=float)
         dists_to_centers = np.sum((X[:, np.newaxis, :] - self.cluster_centers_[np.newaxis, :, :])**2, axis=2)
         return np.argmin(dists_to_centers, axis=1)
@@ -84,6 +89,5 @@ class NumpyKMeans:
     def transform(self, X):
         if self.cluster_centers_ is None:
             raise ValueError("Model not fitted yet. Call fit() first.")
-        
         X = np.array(X, dtype=float)
         return np.sum((X[:, np.newaxis, :] - self.cluster_centers_[np.newaxis, :, :])**2, axis=2)
